999959277f4e1c3e6d067af1b6531fb4c2e61999
Владислав Макарей 850502 <123.vlad.123.vlad.123@gmail.com>
2021-11-25 11:58:33 +0300

    New comment

--- api/src/main/java/com/epam/multi/app/App.java
+++ api/src/main/java/com/epam/multi/app/App.java
@@ -3,6 +3,7 @@ package com.epam.multi.app;
 import com.epam.multi.util.Utils;
 
 public class App {
+    //Hello git
     public static void main(String[] args) {
         System.out.println((Utils.isAllPositiveNumbers("12", "79")));
     }
